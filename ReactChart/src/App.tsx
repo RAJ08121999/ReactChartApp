6dd6ba58d88bc6adbd672d89ee114c694642d074
MD RAJ <rajmd1800@gmail.com>
2025-11-08 01:54:36 +0530

    updated

--- ReactChart/src/App.tsx
+++ ReactChart/src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
     className="max-w-full mx-auto p-6 bg-linear-to-r from-indigo-300 via-purple-300 to-pink-300 rounded-xl shadow-lg animate-fadeIn"
     >
       <TransactionForm/>
-      <div className="max-w-full mx-auto mt-10 p-6 bg-linear-to-r from-indigo-800 via-purple-800 to-pink-800 rounded-xl shadow-lg animate-fadeIn">
+      <div className="max-w-full mx-auto mt-10 bg-linear-to-r from-indigo-800 via-purple-800 to-pink-800 rounded-xl shadow-lg animate-fadeIn">
         <TransactionChart/>
       </div>
       
